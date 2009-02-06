93e321bcd59617c92b0f29c2aa709527d6a9bd9d
jyasskin <jyasskin@b829dee0-eb3e-11dd-80ed-bda8ba2650cd>
2009-02-06 04:56:48 +0000

    Pushed queries into a background thread so the UI doesn't freeze. When an item is selected that will change a subsequent spinner's list, clear that spinner. This makes the UI less confusing and avoids a bug where you'd select a new route and it wouldn't update the stop list. Changed the indentation settings to be a little less Eclipse-specific (4-space tabs?!?) but I'm still using Eclipse's broken Format command, so the formatting isn't great. Made the prediction list redraw every 30s to count down appropriately, and requery from NextMuni every 2 minutes to keep up with any busses that changed speeds. The requery doesn't work yet. Updated the version number to 0.0.1
    git-svn-id: https://droid-muni.googlecode.com/svn/trunk@3 b829dee0-eb3e-11dd-80ed-bda8ba2650cd

--- src/info/yasskin/droidmuni/DroidMuni.java
+++ src/info/yasskin/droidmuni/DroidMuni.java
@@ -1,14 +1,23 @@
 package info.yasskin.droidmuni;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
@@ -16,184 +25,316 @@ import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 public class DroidMuni extends Activity {
-	private int mLine;
-	private int mDirection;
-	private int mStop;
+  private final Handler m_handler = new Handler();
 
-	private SimpleCursorAdapter mLineAdapter;
-	private SimpleCursorAdapter mDirectionAdapter;
-	private SimpleCursorAdapter mStopAdapter;
-	private SimpleCursorAdapter mPredictionsAdapter;
+  private static final ExecutorService s_executor =
+      Executors.newCachedThreadPool();
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+  static final int REDRAW_INTERVAL_MS = 30000;
+  static final int REPREDICT_INTERVAL_MS = 2 * 60000;
 
-		SharedPreferences pref = getPreferences(MODE_PRIVATE);
-		this.mLine = pref.getInt("line", Spinner.INVALID_POSITION);
-		this.mDirection = pref.getInt("direction", Spinner.INVALID_POSITION);
-		this.mStop = pref.getInt("stop", Spinner.INVALID_POSITION);
+  private int m_line;
+  private int m_direction;
+  private int m_stop;
 
-		this.setContentView(R.layout.main);
+  private SimpleCursorAdapter m_line_adapter;
+  private SimpleCursorAdapter m_direction_adapter;
+  private SimpleCursorAdapter m_stop_adapter;
+  private SimpleCursorAdapter m_predictions_adapter;
 
-		Spinner line_spinner = (Spinner) findViewById(R.id.line);
-		mLineAdapter = setupSpinner(line_spinner, "description",
-				mLineClickedHandler);
-		mLineAdapter.changeCursor(managedQuery(NextMuniProvider.ROUTES_URI,
-				null, null, null, null));
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-		Spinner direction_spinner = (Spinner) findViewById(R.id.direction);
-		mDirectionAdapter = setupSpinner(direction_spinner, "title",
-				mDirectionClickedHandler);
+    SharedPreferences pref = getPreferences(MODE_PRIVATE);
+    this.m_line = pref.getInt("line", Spinner.INVALID_POSITION);
+    this.m_direction = pref.getInt("direction", Spinner.INVALID_POSITION);
+    this.m_stop = pref.getInt("stop", Spinner.INVALID_POSITION);
 
-		Spinner stop_spinner = (Spinner) findViewById(R.id.stop);
-		mStopAdapter = setupSpinner(stop_spinner, "title", mStopClickedHandler);
+    this.setContentView(R.layout.main);
 
-		ListView prediction_list = (ListView) findViewById(R.id.predictions);
-		mPredictionsAdapter = new SimpleCursorAdapter(this,
-				android.R.layout.simple_list_item_1,
-				null,
-				// TODO: Show the destination, but only when it's not totally
-				// determined by the route (e.x. 31 Inbound & 38 Outbound).
-				new String[] { "predicted_time" },
-				new int[] { android.R.id.text1 });
-		mPredictionsAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
-			public boolean setViewValue(View view, Cursor cursor,
-					int columnIndex) {
-				if (!(view instanceof TextView)) {
-					return false;
-				}
-				TextView textview = (TextView) view;
-				long expected_arrival = cursor.getLong(columnIndex);
-				long now = System.currentTimeMillis();
-				long delta_minutes = (expected_arrival - now) / 60000;
-				String ago = "";
-				String plural = "";
-				if (delta_minutes < 0) {
-					ago = " ago";
-					delta_minutes = -delta_minutes;
-				}
-				if (delta_minutes != 1) {
-					plural = "s";
-				}
-				textview.setText((expected_arrival - now) / 60000 + " minute"
-						+ plural + ago);
-				return true;
-			}
-		});
-		prediction_list.setAdapter(mPredictionsAdapter);
+    Spinner line_spinner = (Spinner) findViewById(R.id.line);
+    m_line_adapter =
+        setupSpinner(line_spinner, "description", mLineClickedHandler);
+    m_line_adapter.changeCursor(managedQuery(NextMuniProvider.ROUTES_URI, null,
+        null, null, null));
 
-		// if (mLine != Spinner.INVALID_POSITION) {
-		// line_spinner.setSelection(mLine);
-		// }
-		// if (mDirection != Spinner.INVALID_POSITION) {
-		// direction_spinner.setSelection(mDirection);
-		// }
-		// if (mStop != Spinner.INVALID_POSITION) {
-		// stop_spinner.setSelection(mStop);
-		// }
-	}
+    Spinner direction_spinner = (Spinner) findViewById(R.id.direction);
+    m_direction_adapter =
+        setupSpinner(direction_spinner, "title", mDirectionClickedHandler);
 
-	/**
-	 * @param spinner
-	 * @param display_column
-	 *            TODO
-	 * @param selection_handler
-	 *            TODO
-	 * @return
-	 */
-	private SimpleCursorAdapter setupSpinner(Spinner spinner,
-			String display_column, OnItemSelectedListener selection_handler) {
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-				android.R.layout.simple_spinner_item, null,
-				new String[] { display_column },
-				new int[] { android.R.id.text1 });
-		adapter
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner.setAdapter(adapter);
-		spinner.setOnItemSelectedListener(selection_handler);
-		return adapter;
-	}
+    Spinner stop_spinner = (Spinner) findViewById(R.id.stop);
+    m_stop_adapter = setupSpinner(stop_spinner, "title", mStopClickedHandler);
 
-	private final OnItemSelectedListener mLineClickedHandler = new OnItemSelectedListener() {
-		public void onItemSelected(AdapterView<?> parent, View v, int position,
-				long id) {
-			mLine = position;
+    ListView prediction_list = (ListView) findViewById(R.id.predictions);
+    m_predictions_adapter =
+        new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,
+            null,
+            // TODO: Show the destination, but only when it's not
+            // totally
+            // determined by the route (e.x. 31 Inbound & 38
+            // Outbound).
+            new String[] { "predicted_time" }, new int[] { android.R.id.text1 });
+    m_predictions_adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+      public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+        if (!(view instanceof TextView)) {
+          return false;
+        }
+        TextView textview = (TextView) view;
+        long expected_arrival = cursor.getLong(columnIndex);
+        long now = System.currentTimeMillis();
+        long delta_minutes = (expected_arrival - now) / 60000;
+        String ago = "";
+        String plural = "";
+        if (delta_minutes < 0) {
+          ago = " ago";
+          delta_minutes = -delta_minutes;
+        }
+        if (delta_minutes != 1) {
+          plural = "s";
+        }
+        textview.setText(delta_minutes + " minute" + plural + ago);
+        return true;
+      }
+    });
+    prediction_list.setAdapter(m_predictions_adapter);
 
-			Cursor parent_item = (Cursor) parent.getItemAtPosition(position);
-			Log.i("DroidMuni", "Line clicked: "
-					+ DatabaseUtils.dumpCurrentRowToString(parent_item));
-			String selected_route = parent_item.getString(1);
-			Cursor direction_data = managedQuery(Uri.withAppendedPath(
-					NextMuniProvider.DIRECTIONS_URI, selected_route), null,
-					null, null, null);
-			mDirectionAdapter.changeCursor(direction_data);
-			Log.i("DroidMuni", "Directions cursor: "
-					+ DatabaseUtils.dumpCursorToString(mDirectionAdapter
-							.getCursor()));
-		}
+    // if (mLine != Spinner.INVALID_POSITION) {
+    // line_spinner.setSelection(mLine);
+    // }
+    // if (mDirection != Spinner.INVALID_POSITION) {
+    // direction_spinner.setSelection(mDirection);
+    // }
+    // if (mStop != Spinner.INVALID_POSITION) {
+    // stop_spinner.setSelection(mStop);
+    // }
+  }
 
-		public void onNothingSelected(AdapterView<?> parent) {
-			Log.i("DroidMuni", "Nothing selected");
-		}
-	};
+  @Override
+  protected void onStart() {
+    super.onStart();
+    // Start the re-drawing loop for the prediction list.
+    m_handler.postDelayed(mRedrawPredictionList, REDRAW_INTERVAL_MS);
+  }
 
-	private final OnItemSelectedListener mDirectionClickedHandler = new OnItemSelectedListener() {
-		public void onItemSelected(AdapterView<?> parent, View v, int position,
-				long id) {
-			mDirection = position;
+  @Override
+  protected void onResume() {
+    super.onResume();
+    if (this.m_predictions_shown) {
+      m_handler.postDelayed(mRequeryPredictions, 0);
+    }
+  }
 
-			Cursor parent_item = (Cursor) parent.getItemAtPosition(position);
-			Log.i("DroidMuni", "Direction clicked: "
-					+ DatabaseUtils.dumpCurrentRowToString(parent_item));
-			String selected_route = parent_item.getString(1);
-			String selected_direction = parent_item.getString(2);
-			Cursor stop_data = managedQuery(Uri.withAppendedPath(Uri
-					.withAppendedPath(NextMuniProvider.STOPS_URI,
-							selected_route), selected_direction), null, null,
-					null, null);
-			mStopAdapter.changeCursor(stop_data);
-		}
+  @Override
+  public void onPause() {
+    super.onPause();
 
-		public void onNothingSelected(AdapterView<?> parent) {
-			Log.i("DroidMuni", "Nothing selected");
-		}
-	};
+    m_handler.removeCallbacks(mRequeryPredictions);
 
-	private final OnItemSelectedListener mStopClickedHandler = new OnItemSelectedListener() {
-		public void onItemSelected(AdapterView<?> parent, View v, int position,
-				long id) {
-			mStop = position;
+    SharedPreferences.Editor editor = this.getPreferences(MODE_PRIVATE).edit();
+    editor.putInt("line", this.m_line);
+    editor.putInt("direction", this.m_direction);
+    editor.putInt("stop", this.m_stop);
+    editor.commit();
+  }
 
-			Cursor parent_item = (Cursor) parent.getItemAtPosition(position);
-			Log.i("DroidMuni", "Stop clicked: "
-					+ DatabaseUtils.dumpCurrentRowToString(parent_item));
-			String selected_route = parent_item.getString(1);
-			String selected_direction = parent_item.getString(2);
-			String selected_stop = parent_item.getString(3);
-			Cursor prediction_data = managedQuery(Uri.withAppendedPath(
-					NextMuniProvider.PREDICTIONS_URI, selected_route + "/"
-							+ selected_direction + "/" + selected_stop), null,
-					null, null, null);
-			mPredictionsAdapter.changeCursor(prediction_data);
-			Log.i("DroidMuni", "Predictions: "
-					+ DatabaseUtils.dumpCursorToString(prediction_data));
-		}
+  @Override
+  protected void onStop() {
+    super.onStop();
+    // No need to re-draw when we're invisible.
+    m_handler.removeCallbacks(mRedrawPredictionList);
+  }
 
-		public void onNothingSelected(AdapterView<?> parent) {
-			Log.i("DroidMuni", "Nothing selected");
-		}
-	};
+  /**
+   * @param spinner
+   * @param display_column
+   *          TODO
+   * @param selection_handler
+   *          TODO
+   * @return
+   */
+  private SimpleCursorAdapter setupSpinner(Spinner spinner,
+      String display_column, OnItemSelectedListener selection_handler) {
+    SimpleCursorAdapter adapter =
+        new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item,
+            null, new String[] { display_column },
+            new int[] { android.R.id.text1 });
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(adapter);
+    spinner.setOnItemSelectedListener(selection_handler);
+    return adapter;
+  }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		SharedPreferences.Editor editor = this.getPreferences(MODE_PRIVATE)
-				.edit();
-		editor.putInt("line", this.mLine);
-		editor.putInt("direction", this.mDirection);
-		editor.putInt("stop", this.mStop);
-		editor.commit();
-	}
+  /**
+   * Runs a query in the background and returns a Future representing its
+   * result.
+   * 
+   * @see Activity.managedQuery
+   */
+  private Future<Cursor> managedBackgroundQuery(final Uri uri,
+      final String[] projection, final String selection,
+      final String[] selectionArgs, final String sortOrder) {
+    return s_executor.submit(new Callable<Cursor>() {
+      public Cursor call() {
+        return managedQuery(uri, projection, selection, selectionArgs,
+            sortOrder);
+      }
+    });
+  }
+
+  /**
+   * Runs cursor.get() and handles exceptions appropriately for the result of
+   * Activity.managedQuery().
+   */
+  private Cursor getManagedQueryFuture(final Future<Cursor> cursor) {
+    try {
+      return cursor.get();
+    } catch (CancellationException e) {
+      // If the Future was cancelled, don't fill in
+      // the cursor with its result.
+      return null;
+    } catch (InterruptedException e) {
+      // And if the thread was interrupted, just
+      // return.
+      return null;
+    } catch (ExecutionException e) {
+      Log.e("DroidMuni", "managedQuery unexpectedly threw" + " an exception", e);
+      return null;
+    }
+  }
+
+  /**
+   * @param cursor
+   * @param and_then
+   */
+  private void changeCursorWhenReady(final CursorAdapter adapter,
+      final Future<Cursor> cursor) {
+    changeCursorWhenReadyAndThen(adapter, cursor, null);
+  }
+
+  private void changeCursorWhenReadyAndThen(final CursorAdapter adapter,
+      final Future<Cursor> cursor, final Runnable and_then) {
+    s_executor.execute(new Runnable() {
+      public void run() {
+        final Cursor result = getManagedQueryFuture(cursor);
+        if (result != null) {
+          m_handler.post(new Runnable() {
+            public void run() {
+              adapter.changeCursor(result);
+              if (and_then != null) {
+                and_then.run();
+              }
+            }
+          });
+        }
+      }
+    });
+  }
+
+  private final OnItemSelectedListener mLineClickedHandler =
+      new OnItemSelectedListener() {
+        public void onItemSelected(AdapterView<?> parent, View v, int position,
+            long id) {
+          m_line = position;
+
+          Cursor parent_item = (Cursor) parent.getItemAtPosition(position);
+          Log.i("DroidMuni",
+              "Line clicked: "
+                  + DatabaseUtils.dumpCurrentRowToString(parent_item));
+          final String selected_route = parent_item.getString(1);
+          m_direction_adapter.changeCursor(null);
+          Future<Cursor> direction_data =
+              managedBackgroundQuery(Uri.withAppendedPath(
+                  NextMuniProvider.DIRECTIONS_URI, selected_route), null, null,
+                  null, null);
+          changeCursorWhenReady(m_direction_adapter, direction_data);
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+          m_direction_adapter.changeCursor(null);
+        }
+      };
+
+  private final OnItemSelectedListener mDirectionClickedHandler =
+      new OnItemSelectedListener() {
+        public void onItemSelected(AdapterView<?> parent, View v, int position,
+            long id) {
+          m_direction = position;
+
+          Cursor parent_item = (Cursor) parent.getItemAtPosition(position);
+          Log.i("DroidMuni",
+              "Direction clicked: "
+                  + DatabaseUtils.dumpCurrentRowToString(parent_item));
+          String selected_route = parent_item.getString(1);
+          String selected_direction = parent_item.getString(2);
+          m_stop_adapter.changeCursor(null);
+          Future<Cursor> stop_data =
+              managedBackgroundQuery(Uri.withAppendedPath(
+                  NextMuniProvider.STOPS_URI, selected_route + "/"
+                                              + selected_direction), null,
+                  null, null, null);
+          changeCursorWhenReady(m_stop_adapter, stop_data);
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+          m_stop_adapter.changeCursor(null);
+        }
+      };
+
+  private final OnItemSelectedListener mStopClickedHandler =
+      new OnItemSelectedListener() {
+        public void onItemSelected(AdapterView<?> parent, View v, int position,
+            long id) {
+          m_stop = position;
+
+          Cursor parent_item = (Cursor) parent.getItemAtPosition(position);
+          Log.i("DroidMuni",
+              "Stop clicked: "
+                  + DatabaseUtils.dumpCurrentRowToString(parent_item));
+          String selected_route = parent_item.getString(1);
+          String selected_direction = parent_item.getString(2);
+          String selected_stop = parent_item.getString(3);
+          m_predictions_adapter.changeCursor(null);
+          m_predictions_shown = false;
+          m_handler.removeCallbacks(mRequeryPredictions);
+          Future<Cursor> prediction_data =
+              managedBackgroundQuery(Uri.withAppendedPath(
+                  NextMuniProvider.PREDICTIONS_URI, selected_route + "/"
+                                                    + selected_direction + "/"
+                                                    + selected_stop), null,
+                  null, null, null);
+          changeCursorWhenReadyAndThen(m_predictions_adapter, prediction_data,
+              new Runnable() {
+                public void run() {
+                  m_predictions_shown = true;
+                  m_handler.postDelayed(mRequeryPredictions,
+                      REPREDICT_INTERVAL_MS);
+                }
+              });
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+          m_predictions_shown = false;
+          m_handler.removeCallbacks(mRequeryPredictions);
+          m_predictions_adapter.changeCursor(null);
+        }
+      };
+
+  private final Runnable mRedrawPredictionList = new Runnable() {
+    public void run() {
+      findViewById(R.id.predictions).invalidate();
+
+      m_handler.postDelayed(this, REDRAW_INTERVAL_MS);
+    }
+  };
+
+  private boolean m_predictions_shown = false;
+  private final Runnable mRequeryPredictions = new Runnable() {
+    public void run() {
+      m_predictions_adapter.getCursor().requery();
+
+      m_handler.postDelayed(this, REPREDICT_INTERVAL_MS);
+    }
+  };
 }
